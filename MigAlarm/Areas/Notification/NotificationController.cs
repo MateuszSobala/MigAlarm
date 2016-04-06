523e2c2052bdbc649e517937296e0121a918ac98
zlotyloczek <gengarek111@gmail.com>
2016-04-06 22:10:58 +0200

    small fix

--- MigAlarm/Areas/Notification/NotificationController.cs
+++ MigAlarm/Areas/Notification/NotificationController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Data.Entity.Spatial;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Web.Http;
 using MigAlarm.Models;
