6d7004b79b628dca9f637e8bbe26fccbcc010ae6
DeeJay <mrtruskawa@gmail.com>
2016-04-06 18:38:45 +0200

    Removed unused property

--- MigAlarm/Models/Notification.cs
+++ MigAlarm/Models/Notification.cs
@@ -17,7 +17,6 @@ namespace MigAlarm.Models
         [Key]
         public int Id { get; set; }
         public int EventId { get; set; }
-        public int CoordinateId { get; set; }
         public int UserId { get; set; }
 
         [Required]
