ea5c2681d87867c98995b4bbe5012e98e3b1ca48
MateuszSobala <mateusz.sobala@hotmail.com>
2016-04-17 14:26:16 +0200

    dummy EventType enum added

---
+++ MigAlarm/Models/EventType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MigAlarm.Models
+{
+    public enum EventType
+    {
+        Police = 1,
+        Robbery = 2, // rabunek
+        CarAccident = 3, // wypadek samochodowy
+        RoadCollision = 4, // kolizja drogowa
+        CarAccidentWithVictims = 5, // wypadek z ofiarami, parentId = 3
+        FireDepartment = 101, // straż pożarna
+        EmergencyService = 201 // pogotowie
+
+    }
+}
