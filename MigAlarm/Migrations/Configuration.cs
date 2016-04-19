b3ffccbad691892b928065780cceeb9e61aa1ca4
zlotyloczek <gengarek111@gmail.com>
2016-04-19 15:46:28 +0200

    Added new example notification and additional data

--- MigAlarm/Migrations/Configuration.cs
+++ MigAlarm/Migrations/Configuration.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Data.Entity;
 
 namespace MigAlarm.Migrations
@@ -148,7 +149,6 @@ namespace MigAlarm.Migrations
                 Institution = centrumZarzadzaniaKryzysowegoWroclaw,
                 RoleType = RoleType.User
             };
-            defaultUserRole.Users.Add(secondUser);
             context.Roles.AddOrUpdate(secondUserRole);
 
             var defaultAdminRole = new Role
@@ -158,6 +158,100 @@ namespace MigAlarm.Migrations
             };
             defaultAdminRole.Users.Add(defaultAdmin);
             context.Roles.AddOrUpdate(defaultAdminRole);
+
+            var exampleNotification = new Notification
+            {
+                Event = police,
+                EventId = 102,
+                Coordinate = centrumZarzadzaniaKryzysowegoLodzCoordinates,
+                Institution = centrumZarzadzaniaKryzysowegoLodz,
+                UserId = 1,
+            };
+            context.Notifications.Add(exampleNotification);
+
+            var exmapleAdditionalData = new List<AdditionalData>();
+
+            var exampleUserName = new AdditionalData
+            {
+                AdditionalDataType = AdditionalDataType.Name,
+                Text = "Jan Kowalski",
+                Notification = exampleNotification
+            };
+            exmapleAdditionalData.Add(exampleUserName);
+
+            var examplePhoneNumber = new AdditionalData
+            {
+                AdditionalDataType = AdditionalDataType.PhoneNumber,
+                Text = "123456789",
+                Notification = exampleNotification
+            };
+            exmapleAdditionalData.Add(examplePhoneNumber);
+
+
+            var exampleUserYearOfBirth = new AdditionalData
+            {
+                AdditionalDataType = AdditionalDataType.Birthday,
+                Text = "1992",
+                Notification = exampleNotification
+            };
+            exmapleAdditionalData.Add(exampleUserYearOfBirth);
+
+            var exampleUserAddress = new AdditionalData
+            {
+                AdditionalDataType = AdditionalDataType.HomeAddress,
+                Text = "Ul. åmieszna 4\nWroclaw",
+                Notification = exampleNotification
+            };
+            exmapleAdditionalData.Add(exampleUserAddress);
+
+            var exampleUserDiseases = new AdditionalData
+            {
+                AdditionalDataType = AdditionalDataType.Diseases,
+                Text = "Alergia na trawy",
+                Notification = exampleNotification
+            };
+            exmapleAdditionalData.Add(exampleUserDiseases);
+
+            var exampleUserMedicines = new AdditionalData
+            {
+                AdditionalDataType = AdditionalDataType.Medicines,
+                Text = string.Empty,
+                Notification = exampleNotification
+            };
+            exmapleAdditionalData.Add(exampleUserMedicines);
+
+            var exampleUserAppearance = new AdditionalData
+            {
+                AdditionalDataType = AdditionalDataType.Appearance,
+                Text = "Micha≥a Wroc≥awczyka 10\n50-380 Wroc≥aw\n",
+                Notification = exampleNotification
+            };
+            exmapleAdditionalData.Add(exampleUserAppearance);
+
+            var exampleUserBloodGroup = new AdditionalData
+            {
+                AdditionalDataType = AdditionalDataType.BloodGroup,
+                Text = "0",
+                Notification = exampleNotification
+            };
+            exmapleAdditionalData.Add(exampleUserBloodGroup);
+
+            var exampleLocalization = new AdditionalData
+            {
+                AdditionalDataType = AdditionalDataType.Localization,
+                Text = "51.1111787 17.0561944",
+                Notification = exampleNotification
+            };
+            exmapleAdditionalData.Add(exampleLocalization);
+
+            var exampleOther = new AdditionalData
+            {
+                AdditionalDataType = AdditionalDataType.Other,
+                Text = "W domu groüny pies",
+                Notification = exampleNotification
+            };
+            exmapleAdditionalData.Add(exampleOther);
+
             context.SaveChanges();
         }
 
